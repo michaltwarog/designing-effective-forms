143d24e54d6533a9aea483a9513a608ca56db2d7
Michał Twaróg <m.twaroog@gmail.com>
2024-05-19 12:42:23 +0200

    lab3

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let clickCount = 0;
 
-const countryInput = document.getElementById('country');
-const myForm = document.getElementById('form');
-const modal = document.getElementById('form-feedback-modal');
-const clicksInfo = document.getElementById('click-count');
-
-function handleClick() {
+document.addEventListener('click', () => {
     clickCount++;
-    clicksInfo.innerText = clickCount;
-}
+    document.getElementById('click-count').innerText = clickCount;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAndFillCountries();
+    getCountryByIP();
+});
 
 async function fetchAndFillCountries() {
     try {
@@ -18,9 +18,27 @@ async function fetchAndFillCountries() {
         }
         const data = await response.json();
         const countries = data.map(country => country.name.common);
+        const countryInput = document.getElementById('country');
         countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+
+        countryInput.removeEventListener('input', handleCountryInput);
+        countryInput.addEventListener('input', handleCountryInput);
     } catch (error) {
         console.error('Wystąpił błąd:', error);
+    }
+}
+
+function handleCountryInput(event) {
+    const filterValue = event.target.value.toUpperCase();
+    const options = event.target.getElementsByTagName('option');
+
+    for (let i = 0; i < options.length; i++) {
+        const optionValue = options[i].textContent.toUpperCase();
+        if (optionValue.indexOf(filterValue) > -1) {
+            options[i].style.display = '';
+        } else {
+            options[i].style.display = 'none';
+        }
     }
 }
 
@@ -28,8 +46,11 @@ function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
-            const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            const countryName = data.country;
+            const countryInput = document.getElementById('country');
+            countryInput.value = countryName;
+
+            getCountryCode(countryName);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -40,25 +61,62 @@ function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Błąd pobierania danych');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const countryCode = data[0].idd.root + data[0].idd.suffixes.join("");
+            const countryCodeInput = document.getElementById('countryCode');
+            countryCodeInput.value = countryCode;
+        })
+        .catch(error => {
+            console.error('Wystąpił błąd:', error);
+        });
 }
 
+function getInvoiceData() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const street = document.getElementById('street').value;
+    const zipCode = document.getElementById('zipCode').value;
+    const city = document.getElementById('city').value;
 
-(() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
-    document.addEventListener('click', handleClick);
+    const invoiceData = `${firstName} ${lastName}\n${street}\n${zipCode} ${city}`;
 
-    fetchAndFillCountries();
-})()
+    return invoiceData;
+}
+
+document.getElementById('vatUE').addEventListener('change', function () {
+    const vatNumberContainer = document.getElementById('vatNumberContainer');
+    const invoiceDataContainer = document.getElementById('invoiceDataContainer');
+
+    if (this.checked) {
+        vatNumberContainer.style.display = 'block';
+        invoiceDataContainer.style.display = 'block';
+        document.getElementById('invoiceData').value = getInvoiceData();
+    } else {
+        vatNumberContainer.style.display = 'none';
+        invoiceDataContainer.style.display = 'none';
+        document.getElementById('invoiceData').value = '';
+    }
+});
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const modal = new bootstrap.Modal(document.getElementById('form-feedback-modal'));
+    modal.show();
+});
+
+document.getElementById('form-feedback-modal').addEventListener('hidden.bs.modal', function () {
+    window.location.reload();
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('form').dispatchEvent(new Event('submit'));
+    }
+});
